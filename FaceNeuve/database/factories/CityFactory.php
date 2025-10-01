68d4041db7e3728f5b7d8f83d2c91cbfff092225
Danilo Costa <dancc86@gmail.com>
2025-10-01 13:32:37 -0400

    Creation du fichier CityFactory

---
+++ FaceNeuve/database/factories/CityFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\City>
+ */
+class CityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $quebecCities = [
+            'Blainville' => 'BLA',
+            'Boucherville' => 'BOU',
+            'Brossard' => 'BRO',
+            'Châteauguay' => 'CHA',
+            'Dollard-des-Ormeaux' => 'DDO',
+            'Laval' => 'LAV',
+            'Longueuil' => 'LON',
+            'Mirabel' => 'MIR',
+            'Montréal' => 'MTL',
+            'Repentigny' => 'REP',
+            'Saint-Constant' => 'SCO',
+            'Saint-Eustache' => 'SEU',
+            'Saint-Jean-sur-Richelieu' => 'SJR',
+            'Saint-Jérôme' => 'SJE',
+            'Terrebonne' => 'TER'
+        ];
+
+        $city = $this->faker->randomElement(array_keys($quebecCities));
+
+        return [
+            'city' => $city,
+            'abbreviation' => $quebecCities[$city]
+        ];
+    }
+}
