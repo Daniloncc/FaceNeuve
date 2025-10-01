a99842522d9174ec810cdfa9ac69037e1af7516f
Danilo Costa <dancc86@gmail.com>
2025-10-01 13:21:54 -0400

    Alteration le contenu du models/city et corriger le nom de la table dans migrations/students

--- FaceNeuve/database/migrations/2014_10_01_170514_create_students_table.php
+++ FaceNeuve/database/migrations/2014_10_01_170514_create_students_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('student', function (Blueprint $table) {
             $table->id();
             $table->string('firstname', 30);
             $table->string('name', 80);
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->unsignedBigInteger('city_id');
             $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
         });
-
     }
 
     /**
