cc4b8b95cc23296961e3e8578f09719bbe976a79
Danilo Costa <dancc86@gmail.com>
2025-10-01 12:33:53 -0400

    Refaire le fichier migrations/users pour le remettre comme l'original

--- FaceNeuve/database/migrations/2014_10_12_000000_create_users_table.php
+++ FaceNeuve/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,19 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname', 30);
-            $table->string('name', 80);
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone', 20);
-            $table->string('address', 150);
-            // a voir ca
-            $table->date('birthday');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->unsignedBigInteger('city_id');
-            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
         });
     }
 
