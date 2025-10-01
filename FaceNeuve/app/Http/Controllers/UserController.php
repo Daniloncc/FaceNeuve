4aeb13a84217ec2f61dd578661678bc579cdd09d
Danilo Costa <dancc86@gmail.com>
2025-10-01 12:38:57 -0400

    Deleter le models/city et remettre le usercontroller a l'original

--- FaceNeuve/app/Http/Controllers/UserController.php
+++ FaceNeuve/app/Http/Controllers/UserController.php
@@ -1,109 +1,49 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use Illuminate\Http\Request;
-use App\Models\City;
-use App\Models\User;
-use Carbon\Carbon;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class UserController extends Controller
+class User extends Authenticatable
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-        $users = User::select()->orderby('firstname')->get();
-        // print("<pre>");
-        // print_r($users->toArray());
-        // print("</pre>");
-        return view('user.index', ['users' => $users]);
-    }
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * Show the form for creating a new resource.
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
      */
-    public function create()
-    {
-        $cities = City::select(['id', 'city', 'abreviation'])->orderBy('city', 'ASC')->get();
-        // print("<pre>");
-        // print_r($cities->toArray());
-        // print("</pre>");
-
-        return view('user.create', ['cities' => $cities]);
-    }
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     /**
-     * Store a newly created resource in storage.
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
      */
-    public function store(Request $request)
-    {
-        // print_r($request->all());
-        $request->validate([
-            'firstname' => 'required|regex:/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/|max:30',
-            'name' => 'required|regex:/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/|max:80',
-            'email' => 'required|string',
-            'phone' => 'required|regex:/^[0-9]+$/',
-            'address' => 'required|string|max:150',
-            'birthday' => [
-                'required',
-                'date',
-                'before_or_equal:' . Carbon::now()->subYears(16)->format('Y-m-d')
-            ],
-            'password' => 'required|regex:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/|max:150',
-            'city_id' => 'required|integer',
-        ]);
-        //
-
-        $user = User::create([
-            'firstname' => $request->firstname,
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'address' => $request->address,
-            'birthday' => $request->birthday,
-            'password' => $request->password,
-            'city_id' => $request->city_id,
-        ]);
-
-        $users = User::select()->get();
-        // $_SESSION['Success] = 'Task created Successfull'
-        //return view('user.login');
-        return redirect()->route('user.index')->with('message', 'Utilisateur cree avec sucess !');
-        //return $task;
-    }
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
-     * Display the specified resource.
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
      */
-    public function show(string $id)
-    {
-        //
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function tasks()
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return $this->hasMany(Task::class);
     }
 }
