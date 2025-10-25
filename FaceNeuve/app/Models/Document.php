6b8500865a09afec8d29560c1b87068967c67dad
Danilo Costa <dancc86@gmail.com>
2025-10-25 14:48:57 -0400

    Creation du models/document avec les colonnes dans la base de donnes, la function pour encoder et decoder le json pour le systeme 2 langues et la function pour lier les documetens au etudiant qui a fait la publication

--- FaceNeuve/app/Models/Document.php
+++ FaceNeuve/app/Models/Document.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Document extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'file_path',
+        'file_name',
+        'file_type',
+        'student_id',
+        'date'
+    ];
+
+    // Accesseur et mutateur pour 'title' (JSON)
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => json_decode($value, true),
+            set: fn($value) => json_encode($value)
+        );
+    }
+
+    // Relation avec Student
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
