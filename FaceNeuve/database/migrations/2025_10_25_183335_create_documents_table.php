f05d2d99981aa6eea8a1248b933f7b0662365eee
Danilo Costa <dancc86@gmail.com>
2025-10-25 14:36:24 -0400

    Creation des fichiers migrate de models por la nouvelle table Documents

---
+++ FaceNeuve/database/migrations/2025_10_25_183335_create_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->json('title'); 
+            $table->string('file_path'); 
+            $table->string('file_name'); 
+            $table->string('file_type'); 
+            $table->unsignedBigInteger('student_id');
+            $table->date('date');
+            $table->timestamps();
+            
+            $table->foreign('student_id')->references('student_id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
